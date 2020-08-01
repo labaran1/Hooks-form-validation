a2a6a2a32278469912e0c14ccc814dffec193142
labaran1 <adlabaran@gmail.com>
2020-07-31 23:23:40 -0400

    Password validation

--- src/index.js
+++ src/index.js
@@ -1,91 +1,119 @@
 import React from 'react'
-// import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
-
-
-export const validateName= (nameState, fieldName='Name', setNameError, min, max)=> {
-  if (nameState == null || undefined || "") {
-    NameError(`${fieldName} is required`);
+export const validateName = (
+  nameState,
+  fieldName = 'Name',
+  setNameError,
+  min,
+  max
+) => {
+  if (nameState == null || undefined || '') {
+    setNameError(`${fieldName} is required`)
     setTimeout(() => {
-      NameError();
-    }, 5000);
+      setNameError()
+    }, 5000)
   } else if (nameState.length < min) {
-    NameError(`${fieldName} must be at least ${min} characters`);
+    setNameError(`${fieldName} must be at least ${min} characters`)
     setTimeout(() => {
-      NameError();
-    }, 5000);
+      setNameError()
+    }, 5000)
   } else if (nameState.length > max) {
-    NameError(`${fieldName} must be less than ${max} characters`);
+    setNameError(`${fieldName} must be less than ${max} characters`)
     setTimeout(() => {
-      NameError();
-    }, 5000);
+      setNameError()
+    }, 5000)
   } else {
-    return true;
+    return true
   }
-
-
 }
 
-export const validateEmail(emailState, setEmailError)=> {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const validateEmail = (emailState, setEmailError) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  if (emailState == null || undefined || "") {
-    setEmailError("Email is required");
+  if (emailState == null || undefined || '') {
+    setEmailError('Email is required')
     setTimeout(() => {
-      setEmailError();
-    }, 5000);
+      setEmailError()
+    }, 5000)
   } else if (!re.test(emailState.trim())) {
-    setEmailError("Enter a valid Email");
+    setEmailError('Enter a valid Email')
     setTimeout(() => {
-      setEmailError();
-    }, 5000);
+      setEmailError()
+    }, 5000)
   } else {
-    return true;
+    return true
   }
-
 }
 
-export const validatePassword=(passwordState, setPasswordError,min=8, max=32)=>{
-  if (passwordState == null || undefined || "") {
-    setPasswordError("Password is required");
+export const validatePassword = (
+  passwordState,
+  setPasswordError,
+  min = 8,
+  max = 32
+) => {
+  if (passwordState == null || undefined || '') {
+    setPasswordError('Password is required')
     setTimeout(() => {
-      setPasswordError();
-    }, 5000);
+      setPasswordError()
+    }, 5000)
   } else if (passwordState.length < min) {
-    setPasswordError(`Password must be at least ${min} characters`);
+    setPasswordError(`Password must be at least ${min} characters`)
     setTimeout(() => {
-      setPasswordError();
-    }, 5000);
+      setPasswordError()
+    }, 5000)
   } else if (passwordState.length > max) {
-    setPasswordError(`Password must be less than ${max} characters`);
-  }else if(!/[A-Z]/.test(passwordState)) {
+    setPasswordError(`Password must be less than ${max} characters`)
+  } else if (!/[A-Z]/.test(passwordState)) {
     setPasswordError('Password must contain an Upper case letter')
     setTimeout(() => {
-      setPasswordError();
-    }, 5000);
-  }
-  
-  
-  else {
-    return true;
+      setPasswordError()
+    }, 5000)
+  } else if (!/[a-z]/.test(passwordState)) {
+    setPasswordError('Password must contain a lower case letter')
+    setTimeout(() => {
+      setPasswordError()
+    }, 5000)
+  } else if (!/[0-9]/.test(passwordState)) {
+    setPasswordError('Password must contain a number')
+    setTimeout(() => {
+      setPasswordError()
+    }, 5000)
+  } else if (!/[●!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/.test(passwordState)) {
+    setPasswordError('Password must contain a special character')
+    setTimeout(() => {
+      setPasswordError()
+    }, 5000)
+  } else {
+    return true
   }
 }
 
-// export function validateConfirmPassword(password1, password2, passwordError) {
-//   if (password2 == null || undefined || "") {
-//     passwordError("Please Confirm Password");
-//     setTimeout(() => {
-//       passwordError();
-//     }, 5000);
-//   } else if (password2 !== password1) {
-//     passwordError("Password doesn't  match");
-//     setTimeout(() => {
-//       passwordError();
-//     }, 5000);
-//   } else {
-//     return true;
-//   }
-// }
+export function validateConfirmPassword(
+  password1,
+  password2,
+  setConfirmPasswordError
+) {
+  if (password2 == null || undefined || '') {
+    setConfirmPasswordError('Please Confirm Password')
+    setTimeout(() => {
+      setConfirmPasswordError()
+    }, 5000)
+  } else if (
+    password1 == null ||
+    undefined ||
+    ('' && password2 != null) ||
+    undefined
+  ) {
+    setConfirmPasswordError('Please enter password and confirm it ')
+    setTimeout(() => {
+      setConfirmPasswordError()
+    }, 5000)
+  } else if (password2 !== password1) {
+    setConfirmPasswordError("Password doesn't  match")
+    setTimeout(() => {
+      setConfirmPasswordError()
+    }, 5000)
+  } else {
+    return true
+  }
+}
