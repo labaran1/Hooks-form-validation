7a23c27f439dc66d18f8eb7c83865e467a832f15
labaran1 <adlabaran@gmail.com>
2020-07-31 23:51:03 -0400

    Updates

---
+++ precache-manifest.e72d675f6028a32dc3c3f412d471c98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9ea7eb42cf0f31f35a54bc6b4c1b44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca11fcd843c9c177e9ac",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "479ebb1e3476331303d5",
+    "url": "./static/js/2.01f0127d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.01f0127d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca11fcd843c9c177e9ac",
+    "url": "./static/js/main.7a267b0c.chunk.js"
+  },
+  {
+    "revision": "56ba59a8a66ac210caf1",
+    "url": "./static/js/runtime-main.31d494e4.js"
+  }
+]);
